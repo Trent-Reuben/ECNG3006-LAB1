7817f7e0041ea42f98eb5b226b180edf1e7e4d19
Trent Reuben <trent.reuben.3.4@gmail.com>
2022-10-05 20:45:08 -0400

    Modification of gpio code *Compiled

--- gpio/main/user_main.c
+++ gpio/main/user_main.c
@@ -1,12 +1,11 @@
-/* gpio example
+/* 
+Modified gpio example for single input, single output using GPIO2 and GPIO0
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
+Name: Trent Reuben 
+I.D.: 816015091
+Due Date: 7/10/22
 
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
 */
-
 
 #include <stdio.h>
 #include <string.h>
@@ -16,6 +15,8 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 
+#include "freertos/semphr.h"
+
 #include "driver/gpio.h"
 
 #include "esp_log.h"
@@ -23,29 +24,9 @@
 
 static const char *TAG = "main";
 
-/**
- * Brief:
- * This test code shows how to configure gpio and how to use gpio interrupt.
- *
- * GPIO status:
- * GPIO15: output
- * GPIO16: output
- * GPIO4:  input, pulled up, interrupt from rising edge and falling edge
- * GPIO5:  input, pulled up, interrupt from rising edge.
- *
- * Test:
- * Connect GPIO15 with GPIO4
- * Connect GPIO16 with GPIO5
- * Generate pulses on GPIO15/16, that triggers interrupt on GPIO4/5
- *
- */
+#define GPIO_OUTPUT_IO_0    2
 
-#define GPIO_OUTPUT_IO_0    15
-#define GPIO_OUTPUT_IO_1    16
-#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
-#define GPIO_INPUT_IO_0     4
-#define GPIO_INPUT_IO_1     5
-#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
+#define GPIO_INPUT_IO_0     0
 
 static xQueueHandle gpio_evt_queue = NULL;
 
@@ -69,12 +50,13 @@ static void gpio_task_example(void *arg)
 void app_main(void)
 {
     gpio_config_t io_conf;
+    //Set GPIO2 as output with no interrupt 
     //disable interrupt
     io_conf.intr_type = GPIO_INTR_DISABLE;
     //set as output mode
     io_conf.mode = GPIO_MODE_OUTPUT;
-    //bit mask of the pins that you want to set,e.g.GPIO15/16
-    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //bit mask of the pins that you want to set, i.e. GPIO2
+    io_conf.pin_bit_mask = (1ULL<<GPIO_OUTPUT_IO_0);
     //disable pull-down mode
     io_conf.pull_down_en = 0;
     //disable pull-up mode
@@ -82,10 +64,11 @@ void app_main(void)
     //configure GPIO with the given settings
     gpio_config(&io_conf);
 
+    //Set GPIO0 as input with interrupt on falling edge
     //interrupt of rising edge
-    io_conf.intr_type = GPIO_INTR_POSEDGE;
-    //bit mask of the pins, use GPIO4/5 here
-    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    io_conf.intr_type = GPIO_INTR_NEGEDGE;
+    //bit mask of the pins using GPIO0
+    io_conf.pin_bit_mask = (1ULL<<GPIO_INPUT_IO_0);
     //set as input mode
     io_conf.mode = GPIO_MODE_INPUT;
     //enable pull-up mode
@@ -93,7 +76,7 @@ void app_main(void)
     gpio_config(&io_conf);
 
     //change gpio intrrupt type for one pin
-    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
+    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_NEGEDGE);
 
     //create a queue to handle gpio event from isr
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
@@ -104,21 +87,13 @@ void app_main(void)
     gpio_install_isr_service(0);
     //hook isr handler for specific gpio pin
     gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void *) GPIO_INPUT_IO_0);
-    //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void *) GPIO_INPUT_IO_1);
-
-    //remove isr handler for gpio number.
-    gpio_isr_handler_remove(GPIO_INPUT_IO_0);
-    //hook isr handler for specific gpio pin again
-    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void *) GPIO_INPUT_IO_0);
 
     int cnt = 0;
 
     while (1) {
-        ESP_LOGI(TAG, "cnt: %d\n", cnt++);
-        vTaskDelay(1000 / portTICK_RATE_MS);
+        ESP_LOGI(TAG, "cnt: %d\n", cnt++); //count each cycle
+        vTaskDelay(1000 / portTICK_RATE_MS); // 1 second delay 
         gpio_set_level(GPIO_OUTPUT_IO_0, cnt % 2);
-        gpio_set_level(GPIO_OUTPUT_IO_1, cnt % 2);
     }
 }
 
